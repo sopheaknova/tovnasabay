64beed088f3d2b66ed66f813551913bbbac63b7a
Yorn Sovanda <sovanday@yahoo.com>
2015-06-05 16:26:08 +0700

    Fixed Title Content Page

--- templates/posts/content-page.php
+++ templates/posts/content-page.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
 		// Page thumbnail and title.
-		the_title( '<header class="entry-header content-padding-side"><h1 class="entry-title">', '</h1></header><!-- .entry-header -->' );
+		the_title( '<header class="entry-header content-padding-side"><h2 class="entry-title">', '</h2></header><!-- .entry-header -->' );
 	?>
 
 	<div class="entry-content content-padding-side">
