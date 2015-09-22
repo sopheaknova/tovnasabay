51ce98ad54df8d6e0e35feae2d087dc492edb9a2
sovanda <MCR002@MCR002s-iMac.local>
2015-09-22 14:35:29 +0700

    Add Contact form 7 to contact page

--- templates/template-contact.php
+++ templates/template-contact.php
@@ -114,11 +114,7 @@
 				</div><!-- .entry-content -->
 
 
-			<?php	// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-			endwhile;
+			<?php endwhile;
 		?>
 	</div><!-- #main -->
 	<?php get_sidebar();?>
